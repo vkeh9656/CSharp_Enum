88c1c655a1fef9b50d3e74e1029cfa25f1e49bfb
vkeh9656 <37315637+vkeh9656@users.noreply.github.com>
2023-05-08 21:51:29 +0900

    기본 UI 세팅

--- CSharp_Enum/Form1.Designer.cs
+++ CSharp_Enum/Form1.Designer.cs
@@ -28,20 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tboxName = new System.Windows.Forms.TextBox();
+            this.lboxDay = new System.Windows.Forms.ListBox();
+            this.lboxTime = new System.Windows.Forms.ListBox();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.btnResult2 = new System.Windows.Forms.Button();
+            this.tboxResult = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // tboxName
+            // 
+            this.tboxName.Location = new System.Drawing.Point(12, 21);
+            this.tboxName.Name = "tboxName";
+            this.tboxName.Size = new System.Drawing.Size(100, 21);
+            this.tboxName.TabIndex = 0;
+            // 
+            // lboxDay
+            // 
+            this.lboxDay.FormattingEnabled = true;
+            this.lboxDay.ItemHeight = 12;
+            this.lboxDay.Location = new System.Drawing.Point(12, 63);
+            this.lboxDay.Name = "lboxDay";
+            this.lboxDay.Size = new System.Drawing.Size(120, 148);
+            this.lboxDay.TabIndex = 1;
+            // 
+            // lboxTime
+            // 
+            this.lboxTime.FormattingEnabled = true;
+            this.lboxTime.ItemHeight = 12;
+            this.lboxTime.Location = new System.Drawing.Point(160, 63);
+            this.lboxTime.Name = "lboxTime";
+            this.lboxTime.Size = new System.Drawing.Size(120, 148);
+            this.lboxTime.TabIndex = 2;
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(12, 226);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(75, 23);
+            this.btnResult.TabIndex = 3;
+            this.btnResult.Text = "결과보기";
+            this.btnResult.UseVisualStyleBackColor = true;
+            // 
+            // btnResult2
+            // 
+            this.btnResult2.Location = new System.Drawing.Point(117, 226);
+            this.btnResult2.Name = "btnResult2";
+            this.btnResult2.Size = new System.Drawing.Size(130, 23);
+            this.btnResult2.TabIndex = 4;
+            this.btnResult2.Text = "StringFormat Test";
+            this.btnResult2.UseVisualStyleBackColor = true;
+            // 
+            // tboxResult
+            // 
+            this.tboxResult.Enabled = false;
+            this.tboxResult.Location = new System.Drawing.Point(11, 280);
+            this.tboxResult.Multiline = true;
+            this.tboxResult.Name = "tboxResult";
+            this.tboxResult.Size = new System.Drawing.Size(269, 105);
+            this.tboxResult.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(294, 397);
+            this.Controls.Add(this.tboxResult);
+            this.Controls.Add(this.btnResult2);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.lboxTime);
+            this.Controls.Add(this.lboxDay);
+            this.Controls.Add(this.tboxName);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tboxName;
+        private System.Windows.Forms.ListBox lboxDay;
+        private System.Windows.Forms.ListBox lboxTime;
+        private System.Windows.Forms.Button btnResult;
+        private System.Windows.Forms.Button btnResult2;
+        private System.Windows.Forms.TextBox tboxResult;
     }
 }
 
